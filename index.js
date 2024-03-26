25507155fa2e376cee28c0acb0afec1e61648b79
mercynjambi <njambim695@gmail.com>
2024-03-26 08:39:50 +0300

    created arithematic functions

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+// Write yor code here
+
+
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+
+let max = Math.max(1, 4, 12, 20); 
+
+
+
+
